9144d129cbb48c45beb0f20608e7989b3c6cf2da
Rizzle <rza0122@gmail.com>
2023-10-10 22:05:16 -0600

    update text w/button fieldgroup

--- templates/template-parts/sections/section-text-button.php
+++ templates/template-parts/sections/section-text-button.php
@@ -9,10 +9,10 @@ $bg_color = $section['background_color'];
 if ($use_bg_color === 'yes') { $bg_color = ' bg-[' . $bg_color . ']'; } else { $bg_color = '';};
 ?>
 
-<section id="text-container" class="w-full<?php echo $bg_color; ?> flex <?php echo $section['section_layout']; ?>"<?php echo $bg_image; ?>>
+<section id="text-container" class="w-full<?php echo $bg_color; ?>"<?php echo $bg_image; ?>>
     <div class="container-inner">
         <div id="text-image-repeater" class="feature-wrapper">
-            <div class="section-block>">
+            <div class="section-block flex <?php echo $section['section_layout']; ?>">
                 <div id="section-content"  class="section-content" >
                     <?php if ($section['pre_heading']) : ?>
                     <h5><?php echo $section['pre_heading']; ?></h5>
@@ -21,6 +21,8 @@ if ($use_bg_color === 'yes') { $bg_color = ' bg-[' . $bg_color . ']'; } else { $
                     <?php if ($section['content']) : ?>
                     <div class="section-text"><?php echo apply_filters( 'the_content', $section['content'] ) ?></div>
                     <?php endif; ?>
+                </div>
+                <div id="section-buttons">
                     <?php if ($section['button_url']) : ?>
                     <a href="<?php echo $section['button_url']; ?>" class="btn primary" data-lity><?php echo $section['button_text']; ?></a>
                     <?php endif; ?>
